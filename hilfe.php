ad5a717815b2e644cd642335d505ce8a8dd96f10
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-10-30 17:37:45 +0100

    Save 3
    Änderung von HTML PhP Cards gemacht

---
+++ hilfe.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+
+
+<head>
+
+
+
+
+</head>
+<body>
+<h2>FAQ</h2>
+
+</body>
+
+
+
+</html>
+
+
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 
 
-<title>Hilfe</title>
+<title>Startseite</title>
 
 <meta name charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,41 +17,32 @@
 <link rel="stylesheet" href="assets/css/grid.css">
 <link rel="stylesheet" href="assets/css/form.css">
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 
 </head>
 
 <body>
-<nav id="navigation-top">
+    <nav id="navigation-top">
 
-<ul>
-<li> <a href="hilfe.html" target="_blank">Hilfe</a> </li> 
-<li> <a href="registrierung.html">Registrierung</a> </li>
+        <ul>
+            <li> <a href="impressum.php" target="_blank">Impressum</a> </li> 
+            <li> <a href="registrierung.html">Registrierung</a> </li>
+        </ul>
 
-
-</ul>
-
-
+    </nav>
 
 
 
-</nav>
 
-    
-    
+     
 
-
+      
+<?php include 'test.php'; ?>
 
 
 
 
 </body>
-
-
-
-
-
-
-
 </html>
